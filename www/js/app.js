74cedf55795b353cdb971afc5c8621bba860692f
jrburke <jrburke@gmail.com>
2012-03-28 16:09:33 -0700

    Break out the DOM work done for network and appCache into separate modules, as suggested by @digitarald

--- www/js/app.js
+++ www/js/app.js
@@ -1,100 +1,32 @@
 // Main app file.
 
-// To load a submodule for the app, place it in an app/ directory that is a
-// sibling to this file.
+// To load an app-specific module, place it in an app/ directory that is a
+// sibling to this file. See app/uiNetworks.js and app/uiAppCache.js for
+// examples.
 
-// For any third party dependencies, like jQuery,
-// place them in the same directory as this file.
-// This avoids having to do module path configuration.
+// For any third party dependencies, like jQuery, place them in the same
+// directory as this file. This avoids having to do module path configuration,
+// and keeps the third party libraries out of your app/ directory.
 
 /*global window */
 
 define(function (require) {
     'use strict';
 
-    var $ = require('jquery'),
-        appCache = require('appCache'),
-        network = require('network'),
-        networkDom, appCacheStatusDom, appCacheEventDom, updateAlertDom,
-        checkUpdateDom, eventSectionDom;
+    var $ = require('jquery');
 
     // Dependencies that do not have an export of their own, just attach
-    // to other objects, like jQuery.
+    // to other objects, like jQuery. These are just used in the example
+    // bootstrap modal, not directly in the UI for the network and appCache
+    // displays.
     require('bootstrap/modal');
     require('bootstrap/transition');
 
-    // Handles update to the DOM that shows the network state.
-    function updateNetworkDisplay(on) {
-        networkDom.text(on ? 'on' : 'off');
-        networkDom.toggleClass('label-success', on);
-        networkDom.toggleClass('label-important', !on);
-    }
-
-    // Shows updates to appCache state.
-    function updateAppCacheDisplay(eventName, evt) {
-        var message;
-
-        appCacheStatusDom.text(appCache.getStatusName());
-
-        // Make sure the check for update button and event list are visible.
-        checkUpdateDom.show();
-        eventSectionDom.show();
-
-        if (eventName) {
-            if (eventName === 'updateready') {
-                updateAlertDom.show();
-            } else {
-                updateAlertDom.hide();
-            }
-
-            message = eventName;
-            if (eventName === 'progress' && evt.total) {
-                message += ': ' + evt.loaded + ' of ' + evt.total;
-            } else if (eventName === 'error') {
-                message += ': make sure the manifest file is in the correct ' +
-                           'place and .appcache files are served with MIME ' +
-                           'type: text/cache-manifest';
-            }
-            appCacheEventDom.prepend('<div>' + message + '</div>');
-        }
-    }
-
-    // Wait for the DOM to be ready before showing the network state.
+    // Wait for the DOM to be ready before showing the network and appCache
+    // state.
     $(function () {
-        // Display the current network state.
-        networkDom = $('#networkStatus');
-        updateNetworkDisplay(network());
-
-        // Listen for changes in the network.
-        network.on('online', function () {
-            updateNetworkDisplay(true);
-        });
-        network.on('offline', function () {
-            updateNetworkDisplay(false);
-        });
-
-        // Display current appCache state.
-        appCacheStatusDom = $('#appCacheStatus');
-        appCacheEventDom = $('#appCacheEvent');
-        updateAlertDom = $('#updateAlert');
-        checkUpdateDom = $('#checkUpdate');
-        eventSectionDom = $('#eventSection');
-
-        // Listen for any of the appCache events.
-        appCache.eventNames.forEach(function (name) {
-            appCache.on(name, function (evt) {
-                updateAppCacheDisplay(name, evt);
-            });
-        });
-
-        // Wire up appCache Update button.
-        $('#updateButton').bind('click', function (evt) {
-            appCache.swapCache();
-            window.location.reload();
-        });
-
-        $('#checkUpdate').bind('click', function (evt) {
-            appCache.update();
-        });
+        // Enable the UI bindings for the network and appCache displays
+        require('app/uiNetwork')();
+        require('app/uiAppCache')();
     });
 });
