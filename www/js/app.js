30093157a3c782a740ef170ccdf3cd094be13613
jrburke <jrburke@gmail.com>
2012-04-23 16:04:41 -0700

    Moving third party libs to a lib/ and top level entry point sets just one path for app, baseUrl to lib.

--- www/js/app.js
+++ www/js/app.js
@@ -1,33 +1,18 @@
-// Main app file.
-
 // To load an app-specific module, place it in an app/ directory that is a
 // sibling to this file. See app/uiNetworks.js and app/uiAppCache.js for
 // examples.
 
-// For any third party dependencies, like jQuery, place them in the same
-// directory as this file. This avoids having to do module path configuration,
-// and keeps the third party libraries out of your app/ directory.
+// For any third party dependencies, like jQuery, place them in the lib folder.
 
-/*global window */
-
-define(function (require) {
-    'use strict';
-
-    var $ = require('jquery');
-
-    // Dependencies that do not have an export of their own, just attach
-    // to other objects, like jQuery. These are just used in the example
-    // bootstrap modal, not directly in the UI for the network and appCache
-    // displays.
-    require('bootstrap/modal');
-    require('bootstrap/transition');
-
-    // Wait for the DOM to be ready before showing the network and appCache
-    // state.
-    $(function () {
-        // Enable the UI bindings for the network and appCache displays
-        require('app/uiNetwork')();
-        require('app/uiAppCache')();
-        require('app/uiWebAppInstall')();
-    });
+// Configure loading modules from the lib directory,
+// except for 'app' ones, which are in a sibling
+// directory.
+requirejs.config({
+    baseUrl: 'js/lib',
+    paths: {
+        app: '../app'
+    }
 });
+
+// Start loading the main app file.
+requirejs(['app/main']);
---
+++ www/js/app/main.js
@@ -0,0 +1,23 @@
+// Defines the main app module. This one does the top level app wiring.
+
+define(function (require) {
+    'use strict';
+
+    var $ = require('jquery');
+
+    // Dependencies that do not have an export of their own, just attach
+    // to other objects, like jQuery. These are just used in the example
+    // bootstrap modal, not directly in the UI for the network and appCache
+    // displays.
+    require('bootstrap/modal');
+    require('bootstrap/transition');
+
+    // Wait for the DOM to be ready before showing the network and appCache
+    // state.
+    $(function () {
+        // Enable the UI bindings for the network and appCache displays
+        require('./uiNetwork')();
+        require('./uiAppCache')();
+        require('./uiWebAppInstall')();
+    });
+});
